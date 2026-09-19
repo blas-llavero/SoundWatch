3e97e997a99f3f0c7ff47c53eb8f6a2ad0e7a40b
Blas Llavero <8304323+blas-llavero@users.noreply.github.com>
2026-09-19 18:07:42 +0200

    Add in-app help video: app/build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "cat.blas.soundwatch"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.2.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
